14fad2a375b412a9dd0c8d942900c1d44185fcc6
Chris Stevenson <chrislstevenson@gmail.com>
2024-04-14 18:51:55 -0400

    Add resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,63 @@
 export default function Resume() {
   return (
-    <p> My Resume </p>
+    <section className='content'>
+      <p>
+        A <a href='./src/assets/CLS_CV.pdf'>printable CV</a> is also available.
+      </p>
+      <h2 className='is-size-4'>Employment History</h2>
+      <dl>
+        <dt>1999-present</dt>
+        <dd>Associcate Professor of Chemistry and Environmental Studies, University of Richmond</dd>
+
+        <dt>1993--1999</dt>
+        <dd>Assistant Professor of Chemistry, University of Richmond</dd>
+
+        <dt>1991-1993</dt>
+        <dd>Postdoctoral Research Scientist, Oak Ridge National Laboratory</dd>
+      </dl>
+
+      <h2 className='is-size-4'>Education</h2>
+      <dl>
+        <dt>1986-1991</dt>
+        <dd>Ph.D. in Analytical Chemistry, University of Florida</dd>
+
+        <dt>1982-1986</dt>
+        <dd>B.S. in Chemistry, University of North Carolina</dd>
+      </dl>
+
+      <h2 className='is-size-4'>Courses Taught</h2>
+      <ul>
+        <li>Introductory Chemistry (lecture and lab) for STEM majors/minors</li>
+        <li>Pollutant in the Environment (lecture and lab) for non-science majors</li>
+        <li>Environmental Chemistry</li>
+        <li>Analytical Chemistry (lecture and lab)</li>
+        <li>Measurement Statistics</li>
+        <li>Two Physical Chemistry lab courses (Thermodynamics, Kinetics, Quantum Chemistry)</li>
+        <li>Introduction to Environmental Studies</li>
+        <li>Environmental Ethics</li>
+      </ul>
+
+      <h2 className='is-size-4'>Administrative Experience</h2>
+      <ul>
+        <li>Served 10 years as Environmental Studies Program Coordinator</li>
+        <li>Served six years as Chemistry Department Chair</li>
+        <li>Served two years as Geography Department interim Chair</li>
+      </ul>
+
+      <h2 className='is-size-4'>Programming Languages</h2>
+      <p>
+        I have developed significant projects using the following programming languages:
+      </p>
+      <ul>
+        <li>MATLAB</li>
+        <li>R</li>
+        <li>Excel (spreadsheet language and VBA)</li>
+        <li>JavaScript</li>
+        <li>C</li>
+      </ul>
+      <p>
+        I am also expert in the LaTeX typesetting markup language.
+      </p>
+    </section>
   )
 }
