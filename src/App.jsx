482e851e850b10f6fea19b244fd8bdf20af1ed07
Chris Stevenson <chrislstevenson@gmail.com>
2024-04-14 16:07:48 -0400

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <main>
         <Outlet />
       </main>
+
+      <Footer/>
 
     </>
   )
