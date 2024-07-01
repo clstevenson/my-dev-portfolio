f30df84103527e0ac0de01006d33e8d8b56e5893
Chris Stevenson <chrislstevenson@gmail.com>
2024-07-01 04:16:47 -0400

    Common styled components in separate file

---
+++ src/components/MyStyles.jsx
@@ -0,0 +1,15 @@
+/* Shared styled components */
+import styled from "styled-components";
+
+// Prose
+export const ContentWrapper = styled.div`
+  & p {
+    margin: 1rem 0;
+  }
+`;
+
+// Page titles
+export const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 500;
+`;
