bd39915eb6dfa54be6f5485bea6ec72c8c199b3b
Chris Stevenson <chrislstevenson@gmail.com>
2024-07-02 20:30:01 -0400

    Remove Bulma classes from project page
    - improve responsiveness

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,46 +3,122 @@
  */
 
 // component to embed YouTube videos (app demos usually) in an iframe
-import YoutubeEmbed from '../components/YoutubeEmbed';
+import YoutubeEmbed from "../components/YoutubeEmbed";
+import styled from "styled-components";
+import Card from "./Card";
+import { COLORS, QUERIES, WEIGHTS } from "../util/constants";
 
 // local component to return language percentages (as given by GitHub)
 // input is an array of objects, each of which gives the language and its contribution
 const Languages = ({ percentages }) => {
-  const output = percentages.map(lang => (
-    `${Object.keys(lang)}: ${Object.values(lang)}%, `
-  )).join('');
+  const output = percentages
+    .map((lang) => `${Object.keys(lang)}: ${Object.values(lang)}%, `)
+    .join("");
 
-  return <p><strong>Languages</strong>: {output.substring(0, output.length - 2)}</p>
+  return (
+    <p>
+      <strong>Languages</strong>: {output.substring(0, output.length - 2)}
+    </p>
+  );
 };
 
 // now display the info
 export default function Project({ project }) {
   return (
-    <section className="project content columns box my-2">
-      <div className="description column">
-        <h3>{project.title}</h3>
-        <p><strong>Description</strong>: {project.description}</p>
-        <p><strong>Notes</strong>: {project.notes}</p>
-        <ul>
-          <li><a href={project.repoURL} target="_new">Repo</a></li>
-          {project.deployedURL && <li><a href={project.deployedURL} target="_new">Deployed</a></li>}
-          {project.isCollaboration
-           ? <li>Collaboration: {project.myCommits} commits (out of {project.totalCommits} total)</li>
-            : <li>Number of commits: {project.myCommits}</li>
-          }
-        </ul>
-        {/* only display if languages array exists and is not empty */}
-        {project.languagePercentage && project.languagePercentage.length > 0 &&
-          <Languages percentages={project.languagePercentage} />}
-        <p><strong>Tags</strong>: {project.tags.join(', ')}</p>
-      </div>
-      <div className="graphic column">
-        {
-          project.isVideo
-            ? <YoutubeEmbed embedURL={project.graphicURL}/>
-            : <img alt="Project screenshot" src={project.graphicURL} />
-        }
-      </div>
-    </section>
-  )
-};
+    <Card>
+      <Wrapper>
+        <Description>
+          <h3>{project.title}</h3>
+          <p>{project.description}</p>
+          <Details>
+            <p>
+              <strong>Notes</strong>: {project.notes}
+            </p>
+            <ul>
+              <li>
+                <a href={project.repoURL} target="_new">
+                  Repo
+                </a>
+              </li>
+              {project.deployedURL && (
+                <li>
+                  <a href={project.deployedURL} target="_new">
+                    Deployed
+                  </a>
+                </li>
+              )}
+              {project.isCollaboration ? (
+                <li>
+                  Collaboration: {project.myCommits} commits (out of{" "}
+                  {project.totalCommits} total)
+                </li>
+              ) : (
+                <li>Number of commits: {project.myCommits}</li>
+              )}
+            </ul>
+            {/* only display if languages array exists and is not empty */}
+            {project.languagePercentage &&
+              project.languagePercentage.length > 0 && (
+                <Languages percentages={project.languagePercentage} />
+              )}
+            <p>
+              <strong>Tags</strong>: {project.tags.join(", ")}
+            </p>
+          </Details>
+        </Description>
+        <Graphic>
+          {project.isVideo ? (
+            <YoutubeEmbed embedURL={project.graphicURL} />
+          ) : (
+            <ImageLink href={project.deployedURL} target="_new">
+              <img
+                alt={`${project.title} screenshot`}
+                src={project.graphicURL}
+              />
+            </ImageLink>
+          )}
+        </Graphic>
+      </Wrapper>
+    </Card>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 16px;
+
+  @media ${QUERIES.mobile} {
+    flex-direction: column-reverse;
+  }
+`;
+
+const Description = styled.div`
+  flex-basis: 40%;
+  flex-shrink: 1;
+`;
+
+const Graphic = styled.div`
+  flex-basis: 60%;
+  flex-shrink: 1;
+  overflow: hidden;
+  outline-offset: 2px;
+
+  &:hover {
+    outline: 2px solid ${COLORS.gray[500]};
+  }
+`;
+
+const ImageLink = styled.a`
+  &:hover img {
+    transform-origin: 50% 0%;
+    transform: scale(1.1);
+    transition: transform 250ms;
+  }
+`;
+
+const Details = styled.div`
+  @media ${QUERIES.tabletAndLess} {
+    display: none;
+  }
+`;
--- src/components/YoutubeEmbed.jsx
+++ src/components/YoutubeEmbed.jsx
@@ -1,17 +1,35 @@
 // local component to embed a YouTube video demo
 // input is the "embed URL" copied from YT
 // Adapted from https://dev.to/bravemaster619/simplest-way-to-embed-a-youtube-video-in-your-react-app-3bk2
+import styled from "styled-components";
 
 export default function YoutubeEmbed({ embedURL }) {
   return (
-    <div className="video-responsive">
-      <iframe width="560" height="315"
+    <VideoWrapper>
+      <iframe
+        width="560"
+        height="315"
         src={embedURL}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
-    </div>
-  )
+    </VideoWrapper>
+  );
 }
+
+const VideoWrapper = styled.div`
+  overflow: hidden;
+  padding-bottom: 56.25%;
+  position: relative;
+  height: 0;
+
+  & iframe {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+  }
+`;
