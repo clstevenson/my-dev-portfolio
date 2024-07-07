056e7cf4479f156f7015edda6680835084a7c1af
Chris Stevenson <chrislstevenson@gmail.com>
2024-07-07 13:13:41 -0400

    Add Toast animations
    - slide-in and fade-out
    - also some minor adjustments of appearance

--- client/src/components/ToastMessage.jsx
+++ client/src/components/ToastMessage.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import * as Toast from "@radix-ui/react-toast";
 import { COLORS } from "../util/constants";
 
@@ -24,23 +24,44 @@ const ToastMessage = ({
   );
 };
 
+const fade = keyframes`
+  from {opacity: 1;}
+  to {opacity: 0;}
+`;
+
+const appear = keyframes`
+  from { transform: translateX(calc(100% + var(--viewport-padding)));}
+  to { transform: translateX(0);}
+`;
+
 const ToastViewport = styled(Toast.Viewport)`
+  --viewport-padding: 24px;
   position: absolute;
   bottom: 0;
   right: 0;
-  margin: 24px;
+  padding: var(--viewport-padding);
   list-style: none;
+  overflow: hidden;
+  z-index: 100;
 `;
 
 const ToastRoot = styled(Toast.Root)`
   display: block;
   background-color: ${COLORS.primary_light};
   color: ${COLORS.white};
-  border-radius: 8px;
-  padding: 16px;
+  border-radius: 4px;
+  padding: 8px 12px;
   box-shadow: 2px 4px 8px ${COLORS.gray[700]};
   border: 1px solid black;
   font-size: 1.2rem;
+
+  &[data-state="open"] {
+    animation: ${appear} 200ms;
+  }
+
+  &[data-state="closed"] {
+    animation: ${fade} 200ms ease-in;
+  }
 `;
 
 export default ToastMessage;
