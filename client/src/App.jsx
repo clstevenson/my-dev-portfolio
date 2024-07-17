75405d4d5c160eebfc36b2fdecf64c024191c73a
Chris Stevenson <chrislstevenson@gmail.com>
2024-07-17 11:22:52 -0400

    Add minor comment/instruction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,8 @@ function App() {
 
 const Wrapper = styled.div`
   min-height: 100%;
+  /* uncomment the next line if you want footer always visible */
+  /* (also uncomment the overflow:auto rule in "Main") */
   /* max-height: 100%; */
   display: flex;
   flex-direction: column;
